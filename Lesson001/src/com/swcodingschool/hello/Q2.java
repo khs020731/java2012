805dbdecaa0aaa5fd87bfea889d92c92a7d1d51e
khs020731 <76715560+khs020731@users.noreply.github.com>
2020-12-30 11:13:57 +0900

    2020-12-30 11:13

---
+++ Lesson001/src/com/swcodingschool/hello/Q2.java
@@ -0,0 +1,14 @@
+package com.swcodingschool.hello;
+
+import java.util.Scanner;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);	
+		double radius = sc.nextDouble();
+		System.out.printf("%.2f",  radius*radius*3.14);
+	}
+
+}
