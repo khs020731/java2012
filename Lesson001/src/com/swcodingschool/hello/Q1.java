e1cf44e3e3076197ca8fe24568e189f7e4b107c4
khs020731 <76715560+khs020731@users.noreply.github.com>
2020-12-30 10:22:25 +0900

    2020-12-30 10:20

--- Lesson001/src/com/swcodingschool/hello/Q1.java
+++ Lesson001/src/com/swcodingschool/hello/Q1.java
@@ -6,11 +6,14 @@ public class Q1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.printf("점수를 입력하세요 : ");
 		int score = sc.nextInt();
+		System.out.printf("성적은 ");
 		if(score>=90) System.out.printf("A");
 		else if (score>=80)System.out.printf("B");
 		else if (score>=70)System.out.printf("C");
 		else System.out.printf("F");
+		
 		
 		switch((int)score/10) {
 			case 10 : case 9 : System.out.printf("A");break;
