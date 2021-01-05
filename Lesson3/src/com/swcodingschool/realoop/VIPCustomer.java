da52499e352fec3d6e7142ae207a5d010db67416
khs020731 <76715560+khs020731@users.noreply.github.com>
2021-01-05 16:46:36 +0900

    2021-01-05 16:46

--- Lesson3/src/com/swcodingschool/realoop/VIPCustomer.java
+++ Lesson3/src/com/swcodingschool/realoop/VIPCustomer.java
@@ -10,6 +10,18 @@ public class VIPCustomer extends Customer {
 		System.out.println("VIPcustomer 생성자 동작...");
 	}
 	
+	
+	
+	@Override
+	public int calcPrice(int price) {
+		// TODO Auto-generated method stub
+		//return super.calcPrice(price);
+		bonusPoint += price * bonusRatio;
+		return price - price * (int)saleRatio;
+	}
+
+
+
 	public int getAgentID() {
 		return agentID;
 	}
